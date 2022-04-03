0ec7daa2b96eb20a81e194dc743980b13041b7e1
ATISH01 <ab.atish.ab@gmail.com>
2022-04-03 22:18:08 +0600

    all component created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-light bg-light">
+                <div class="container-fluid">
+                    <a className="navbar-brand">Navbar</a>
+                    <form class="d-flex">
+                        <div className='nav fs-5'>
+                            <Link to="/">Home</Link>
+                            <Link to="/review">Reviews</Link>
+                            <Link to="blogs">Blogs</Link>
+                            <Link to="about">About</Link>
+                        </div>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h1>revies</h1>
+        </div>
+    );
+};
+
+export default Reviews;
