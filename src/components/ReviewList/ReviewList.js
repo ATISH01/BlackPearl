ee5a6b8aab549201a8227971a17d212eb2633884
ATISH01 <ab.atish.ab@gmail.com>
2022-04-04 00:08:39 +0600

    review component and hook

--- src/components/ReviewList/ReviewList.js
+++ src/components/ReviewList/ReviewList.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const ReviewList = () => {
+const ReviewList = (props) => {
+    const { name, img, review,ratings }=props.reviewList;
     return (
         <div>
-            
+            <div className="col">
+                <div className="card w-75">
+                    <div class="card-body">
+                        <h5 class="card-title">Name:{name}</h5>
+                        <p>Review:{review}</p>
+                        <p>Ratings:{ratings}</p>
+                    </div>
+                    <img className='p-3 rounded' src={img} class="card-img-top" alt="..." />
+
+                </div>
+
+            </div>
         </div>
     );
 };
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import useReview from '../../DataHook/DataHook';
+import ReviewList from '../ReviewList/ReviewList';
+
+
+const Reviews = () => {
+    const [reviews,setReviews]=useReview([]);
+    return (
+        <div className="p-3 row row-cols-1 row-cols-md-3 g-4">
+            {
+                reviews.map(reviewList=><ReviewList reviewList={reviewList}></ReviewList>)
+            }
+        </div>
+    );
+};
+
+export default Reviews;
